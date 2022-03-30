010ab7665eb346fd0e35b0f3ac026fb1a9382229
VladimirovES <vlad.ev@mail.ru>
2022-03-30 22:46:15 +0300

    Ading game results to DataFrame

--- LichAnalyse.py
+++ LichAnalyse.py
@@ -1,8 +1,18 @@
+from urllib import response
 import requests
+import ndjson
 
 with open('./lichess.token') as f:
     token = f.read()
 
-hed = requests.headers = {'Authorization': f'Bearer {token}'}
+class LichAnalyse:   
 
-print(requests.get("https://lichess.org/api/account", headers=hed).json())
+    def __init__(self, token):
+        self.hed = requests.headers = {'Authorization': f'Bearer {token}', 'Accept': 'application/x-ndjson'}
+        self.token = token
+        self.res = requests.get("https://lichess.org/api/account", headers=self.hed).json()
+        self.account_name = self.res['username']
+
+    def get_games(self):
+        response = requests.get(f"https://lichess.org/api/games/user/{self.account_name}", headers=self.hed).json(cls=ndjson.Decoder)
+        return response
